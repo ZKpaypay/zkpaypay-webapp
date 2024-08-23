bd77772b29ae151bbfea7f5a13a1c4d599cb8d63
sey323 <sey323_nm@yahoo.co.jp>
2024-08-24 00:31:23 +0900

    :sparkles: logout and redirect login page

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -1,10 +1,22 @@
+"use client";
 import "@/app/globals.css";
+import { Providers } from "@/utils/providers";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
 
 function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
-      <body>{children}</body>
+      <body>
+        <Providers>
+          <main className="mx-8 flex justify-center flex-col">
+            <div className="flex justify-end">
+              <ConnectButton />
+            </div>
+            {children}
+          </main>
+        </Providers>
+      </body>
     </html>
   );
 }
--- app/(authenticated)/page.tsx
+++ app/(authenticated)/page.tsx
@@ -1,10 +1,25 @@
 "use client";
 
+import { useAccount } from "wagmi";
+
 function Page() {
+  const account = useAccount();
+
+  // isConnectedがfalseの時/loginのページにリダイレクト
+  if (!account.isConnected) {
+    window.location.href = "/login";
+  }
+
   return (
     <div>
-      <div className="flex justify-center">
+      <div className="flex justify-center items-center flex-col">
         <h1>zkppay</h1>
+        {account.isConnected && (
+          <div>
+            <div>接続中のアドレス: {account.address}</div>
+            <div>{account.chain?.name}に接続中</div>
+          </div>
+        )}
       </div>
     </div>
   );
