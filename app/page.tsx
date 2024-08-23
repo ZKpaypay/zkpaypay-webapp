bb55eeb710fcda43ca7e7b30ff563e6c324db39a
sey323 <sey323_nm@yahoo.co.jp>
2024-08-23 20:31:33 +0900

    :fire: unuse state and abi

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,6 @@
 "use client";
-import { useState } from "react";
 
 function Page() {
-  // textのinputの値を取得
-  const [contractAddress, setContractAddress] = useState(
-    "0x3db5bb7de1e85fabef286cc03c8a46787d60d98c" as `0x${string}`
-  );
-
   return (
     <div>
       <div className="flex justify-center">
