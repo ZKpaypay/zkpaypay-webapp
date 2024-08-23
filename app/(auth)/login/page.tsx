536f22db2f41fa9d72e8a0c4159e1998d2d148f8
sey323 <sey323_nm@yahoo.co.jp>
2024-08-24 00:30:30 +0900

    :sparkles: login and redirct root page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,7 @@
 import { Providers } from "@/utils/providers";
 import LoginHeader from "../../_components/login-header";
-import { ConnectButton } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
+import LoginButton from "@/app/_components/login-button";
 
 export default function Page() {
   return (
@@ -9,16 +9,16 @@ export default function Page() {
       <div className="mb-4">
         <LoginHeader></LoginHeader>
       </div>
-      <h1 className="text-4xl font-normal leading-[36px] text-left text-[#0027F3]">
+      <div className="text-4xl font-normal leading-[36px] text-left text-[#0027F3]">
         ZKPay
-      </h1>
+      </div>
       <div className="min-h-[70vh] flex justify-center items-center flex-col">
         <div className="mb-4">
           <span className="text-[#0027F3]">Connect your wallet</span>
         </div>
         <div>
           <Providers>
-            <ConnectButton />
+            <LoginButton />
           </Providers>
         </div>
       </div>
