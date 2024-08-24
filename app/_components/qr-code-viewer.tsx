07eff9a22c0f231524ef89aa768e6ca6ef5b5de7
sey323 <sey323_nm@yahoo.co.jp>
2024-08-24 15:26:26 +0900

    :bug: add margin

--- app/_components/qr-code-viewer.tsx
+++ app/_components/qr-code-viewer.tsx
@@ -2,14 +2,16 @@ import { QRCodeCanvas } from "qrcode.react";
 
 export default function QrCodeViewer({ address }: { address: string }) {
   return (
-    <QRCodeCanvas
-      value={address}
-      style={{
-        width: "50vw",
-        height: "50vw",
-        maxWidth: "400px",
-        maxHeight: "400px",
-      }}
-    ></QRCodeCanvas>
+    <div className="p-8 bg-white">
+      <QRCodeCanvas
+        value={address}
+        style={{
+          width: "50vw",
+          height: "50vw",
+          maxWidth: "400px",
+          maxHeight: "400px",
+        }}
+      ></QRCodeCanvas>
+    </div>
   );
 }
