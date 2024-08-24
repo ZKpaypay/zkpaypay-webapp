ccb1cdfcd5174312b56cb5c212045672aad5cefc
sey323 <sey323_nm@yahoo.co.jp>
2024-08-24 14:46:55 +0900

    :bug: modalStyle type fetch error

--- app/_components/wallet-address-field.tsx
+++ app/_components/wallet-address-field.tsx
@@ -5,13 +5,11 @@ import QrCodeViewer from "./qr-code-viewer";
 
 const modalStyle = {
   overlay: {
-    position: "fixed",
     top: 0,
     left: 0,
     backgroundColor: "rgba(0,0,0,0.85)",
   },
   content: {
-    position: "absolute",
     top: "5rem",
     left: "5rem",
     right: "5rem",
