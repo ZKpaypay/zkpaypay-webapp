37f044ad2a4613981d1d92b1fd5149d1cc79ac61
sey323 <sey323_nm@yahoo.co.jp>
2024-08-24 15:09:16 +0900

    :art: fix qr code size

--- app/_components/qr-code-viewer.tsx
+++ app/_components/qr-code-viewer.tsx
@@ -4,7 +4,12 @@ export default function QrCodeViewer({ address }: { address: string }) {
   return (
     <QRCodeCanvas
       value={address}
-      style={{ width: "100%", height: "100%" }}
+      style={{
+        width: "50vw",
+        height: "50vw",
+        maxWidth: "400px",
+        maxHeight: "400px",
+      }}
     ></QRCodeCanvas>
   );
 }
